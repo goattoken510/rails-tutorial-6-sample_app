b4dc8a9dc6733314b0b5abec1b6cc8cf131e4dc6
goattoken510 <goattoken510@gmail.com>
2020-09-19 09:48:21 +0000

    Add friendly forwarding

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,39 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+  
   test "should get new" do
     get signup_path
     assert_response :success
+  end
+  
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect update when not logged id" do
+    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect edit when not logged in as wrong user" do
+    get edit_user_path(@other_user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect update when not logged id as wrong user" do
+    patch user_path(@other_user), params: { user: { name: @user.name, email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 
 end
